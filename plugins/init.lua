eebaaa21df3cf34b7c98ef21322034e4ba9cc09d
yawarpwn <john.kazz@gmail.com>
2022-11-03 20:15:33 -0500

    change to tokyonight colorscheme

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,5 +1,6 @@
 return {
-  ["EdenEast/nightfox.nvim"] = { config = function() require "user.plugins.nightfox" end },
+  -- ["EdenEast/nightfox.nvim"] = { config = function() require "user.plugins.nightfox" end },
+  ["folke/tokyonight.nvim"] = { config = function() require("tokyonight").setup({}) end },
   ["hrsh7th/cmp-emoji"] = { after = "nvim-cmp", config = function() require "user.plugins.cmp-emoji" end },
   ["kylechui/nvim-surround"] = { tag = "*", config = function() require "user.plugins.nvim-surround" end },
   ["nvim-treesitter/nvim-treesitter-textobjects"] = { after = "nvim-treesitter" },
